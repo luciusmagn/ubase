cae111d5be76eee9df78d2f6c92b35033e4aa18f
Lukáš Hozda <luk.hozda@gmail.com>
2017-09-13 01:10:00 +0200

    enable optimizations for file size

--- config.mk
+++ config.mk
@@ -10,6 +10,6 @@ AR = ar
 RANLIB = ranlib
 
 CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -std=c99 -Wall -Wextra
+CFLAGS   = -std=c99 -Wall -Wextra -Os
 LDLIBS   = -lcrypt
 LDFLAGS  = -s
