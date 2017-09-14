bf7e860ef90913adff9b2d33c49b8f1e2e49f9e1
Lukáš Hozda <luk.hozda@gmail.com>
2017-09-14 08:51:39 +0200

    enable static compilation and size optimizations

--- config.mk
+++ config.mk
@@ -5,11 +5,10 @@ VERSION = 0.1
 PREFIX = /usr/local
 MANPREFIX = $(PREFIX)/share/man
 
-CC = cc
 AR = ar
 RANLIB = ranlib
 
 CPPFLAGS = -D_FILE_OFFSET_BITS=64 -D_XOPEN_SOURCE=700 -D_GNU_SOURCE
-CFLAGS   = -std=c99 -Wall -Wextra -Os
 LDLIBS   = -lcrypt
-LDFLAGS  = -s
+CFLAGS   = -std=c99 -pedantic -Wall -Os -static -fPIC -lrt -static -ffunction-sections -fdata-sections
+LDFLAGS  = -s -static -Wl,--gc-sections
